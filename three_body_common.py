4cdcaf524c122071216b64bcd89891f83ca96a6b
Eric Moyer <compu.moyer@gmail.com>
2024-07-13 10:11:23 -0400

    Use the inverse cube expression for force components
    This is a small optimization. Also rename "epsilon" to "gravity_y_strength".

--- three_body_common.py
+++ three_body_common.py
@@ -26,7 +26,7 @@ class SimulationParams:
 
 
 def modified_three_body(
-    _: float, state: np.ndarray, epsilon: float, gravity: float = 1
+    _: float, state: np.ndarray, gravity_y_strength: float, gravity: float = 1
 ) -> np.ndarray:
     """
     Compute the derivatives for the modified three-body problem.
@@ -34,7 +34,7 @@ def modified_three_body(
     Args:
         _: Current time (unused, but required by solve_ivp).
         state: Current state [x1, y1, vx1, vy1, x2, y2, vx2, vy2, x3, y3, vx3, vy3].
-        epsilon: Coupling strength for the y-dimension.
+        gravity_y_strength: Coupling strength for the y-dimension.
         gravity: Gravitational constant. Defaults to 1.
 
     Returns:
@@ -45,20 +45,22 @@ def modified_three_body(
 
     # Distance is the same, the only change is the magnitude
     # of the force in the y-direction
-    # rXYsq is the squared distance between bodies X and Y
     # rXY is the distance between bodies X and Y
-    r12sq = (x1 - x2) ** 2 + (y1 - y2) ** 2
-    r12 = np.sqrt(r12sq)
-    r13sq = (x1 - x3) ** 2 + (y1 - y3) ** 2
-    r13 = np.sqrt(r13sq)
-    r23sq = (x2 - x3) ** 2 + (y2 - y3) ** 2
-    r23 = np.sqrt(r23sq)
+    r12 = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    r13 = np.sqrt((x1 - x3) ** 2 + (y1 - y3) ** 2)
+    r23 = np.sqrt((x2 - x3) ** 2 + (y2 - y3) ** 2)
+
+    # Calculate the cubes (r**3) of the distances because
+    # of the cubic term that appears later on
+    r12_cube = r12**3
+    r13_cube = r13**3
+    r23_cube = r23**3
 
     # Magnitude of the force between bodies
-    f12 = gravity / r12sq
-    f13 = gravity / r13sq
-    f23 = gravity / r23sq
-
+    # f12 = gravity / (r12 ** 2)
+    # f13 = gravity / (r13 ** 2)
+    # f23 = gravity / (r23 ** 2)
+    #
     # Now, multiply by the appropriate trig function of the angle
     # to get components of the forces between the bodies
     # magnitude * sin(angle) = y-component
@@ -67,12 +69,22 @@ def modified_three_body(
     # cos(angle) = adjacent / hypotenuse = delta_x / r
     # I don't take the absolute value of the delta_x and delta_y
     # to preserve the direction of the force.
-    f12_x = f12 * (x2 - x1) / r12
-    f12_y = epsilon * f12 * (y2 - y1) / r12
-    f13_x = f13 * (x3 - x1) / r13
-    f13_y = epsilon * f13 * (y3 - y1) / r13
-    f23_x = f23 * (x3 - x2) / r23
-    f23_y = epsilon * f23 * (y3 - y2) / r23
+    #
+    # When you calculate the force components, you get an
+    # r**3 in the denominator, to change the distance term
+    # in the numerator into the fraction of the force along
+    # the appropriate axis.
+    #
+    # f12_x = f12 * (x2 - x1) / r12 and substitute f12
+    #       = (gravity / (r12 ** 2)) * (x2 - x1) / r12
+    #       = gravity * (x2 - x1) / (r12 ** 3)
+    f12_x = gravity * (x2 - x1) / r12_cube
+    # Similarly for y, but multiply by gravity_y_strength
+    f12_y = gravity_y_strength * gravity * (y2 - y1) / r12_cube
+    f13_x = gravity * (x3 - x1) / r13_cube
+    f13_y = gravity_y_strength * gravity * (y3 - y1) / r13_cube
+    f23_x = gravity * (x3 - x2) / r23_cube
+    f23_y = gravity_y_strength * gravity * (y3 - y2) / r23_cube
 
     # Acceleration is force divided by mass (which is 1)
     # so sum the forces to get the acceleration
